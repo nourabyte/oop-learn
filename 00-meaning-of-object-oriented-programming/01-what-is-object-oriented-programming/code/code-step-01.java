b75f08c32c5cdec36e03b94882f6fbb3693fc1dc
nouraop <148945652+nouraop@users.noreply.github.com>
2023-10-26 11:45:09 +0300

    Update code-step-01.java

--- 00-meaning-of-object-oriented-programming/01-what-is-object-oriented-programming/code/code-step-01.java
+++ 00-meaning-of-object-oriented-programming/01-what-is-object-oriented-programming/code/code-step-01.java
@@ -1,1 +1,20 @@
 
+```java
+//class creation
+
+public class Number {
+
+int y = 10;
+
+public static void main(String[] args) {
+
+//object creation
+
+Number Obj1 = new Number(); 
+
+System.out.println(Obj1.y);
+
+}
+}
+```
+
