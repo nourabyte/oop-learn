e173517980d442f6f7c4b1ae7ffec4da7ba0cc5c
nouraop <148945652+nouraop@users.noreply.github.com>
2023-11-03 18:40:40 +0300

    Update example-step-01.java

--- 01-Class/01-data-types/code/example-step-01.java
+++ 01-Class/01-data-types/code/example-step-01.java
@@ -1,1 +1,17 @@
 
+
+public class DataTypesExample {
+    public static void main(String[] args) {
+        /* Primitive data types */
+        int age = 25;
+        double salary = 2500.50;
+        boolean isEmployed = true;
+        char grade = 'A';
+        byte num = 100;
+        short count = 500;
+        long population = 9876543210L;
+
+        /* Non-primitive data types */
+        String name = "John Doe";
+        int[] numbers = {1, 2, 3, 4, 5};
+}}
