145d51171b3e8f931701627d3dd8d42b34c87f35
nouraop <148945652+nouraop@users.noreply.github.com>
2023-11-04 14:42:13 +0300

    Update practice-step-02.java

--- 02-nheritance/02-inheritance-with-access-modifiers/code/practice-step-02.java
+++ 02-nheritance/02-inheritance-with-access-modifiers/code/practice-step-02.java
@@ -1,1 +1,17 @@
 
+## Practice
+1. is the below code written correctly?
+```java
+package pack1;
+ class A{
+     } 
+package pack2;
+class B extends A{     
+    }
+```
+
+
+/* the answer is 
+No. Class with default (no) access modifiers can not have subclass 
+outside the package.
+*/
