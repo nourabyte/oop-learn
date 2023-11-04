4eb72b090e1ed831aa7d0038a6e4a787ae63199d
nouraop <148945652+nouraop@users.noreply.github.com>
2023-11-04 14:40:29 +0300

    Update example-step-02.java

--- 02-nheritance/02-inheritance-with-access-modifiers/code/example-step-02.java
+++ 02-nheritance/02-inheritance-with-access-modifiers/code/example-step-02.java
@@ -1,1 +1,17 @@
+//Private
+class A {
+  int i;
+  private int j; //private to A.
+  void setA (int x,int y){
+i = x;
+j = y;
+  }
+}
+class B extends A{
+double total;
+void sum(){
+total = i+j;
 
+// ERROR, j not accessible here
+  }
+}
