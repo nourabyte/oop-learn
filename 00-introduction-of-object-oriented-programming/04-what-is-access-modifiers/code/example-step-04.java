3f1e353611e4b3d92a511478e4c252e6bf4b16d0
nouraop <148945652+nouraop@users.noreply.github.com>
2023-11-05 11:27:51 +0300

    Update example-step-04.java

--- 00-introduction-of-object-oriented-programming/04-what-is-access-modifiers/code/example-step-04.java
+++ 00-introduction-of-object-oriented-programming/04-what-is-access-modifiers/code/example-step-04.java
@@ -1,1 +1,31 @@
+- Public
+```java
+public class Person {
+    public String name;
+    public void greet() {
+        System.out.println("Hello, I'm " + name + "!");  }}
+```
+- Private 
+```java
+public class BankAccount {
+    private double balance;
+    private void deductFees() {
+      }}
+```
+- Protected
+```java
+public class Animal {
+    protected String name;
+    protected void sleep() {
+        System.out.println("The animal is sleeping.");  }}
+```
+- Default
+```java
+/* Default */
+class Person {
+    String name;
+    void greet() {
+        System.out.println("Hello, I'm " + name + "!");  }}
+```
+
 
