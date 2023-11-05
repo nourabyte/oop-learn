2e77f8ac9a17453717815f0f250ba3c6e05be6df
nouraop <148945652+nouraop@users.noreply.github.com>
2023-11-05 10:22:17 +0300

    Update Example-step-02.java

--- 00-introduction-of-object-oriented-programming/02-what-is-a-class/Code/Example-step-02.java
+++ 00-introduction-of-object-oriented-programming/02-what-is-a-class/Code/Example-step-02.java
@@ -1,1 +1,11 @@
+/* 1. Create new class by using (class) keyword. */
+public class Student {
+ /* 2. Attributes*/
+    public String name;
+    public int age;
+    public int gradeLevel;
 
+/* 3. Method */
+    public void attendence() {
+    }
+}
